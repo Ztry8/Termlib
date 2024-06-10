36cfabeb0d6d0e586a41d20a151eeca1022e0709
AslD <122040070+Ztry8@users.noreply.github.com>
2024-06-10 19:43:25 +0600

    Update core.h

--- core.h
+++ core.h
@@ -1,22 +1,22 @@
 #pragma once
 #include <SDL.h>
 
-#define BLACK 0
-#define BLUE 1
-#define GREEN 2
-#define CYAN 3
-#define RED 4
-#define MAGENTA 5
-#define BROWN 6
-#define LIGHT_GRAY 7
-#define DARK_GRAY 8
-#define BRIGHT_BLUE 9
-#define BRIGHT_GREEN 10
-#define BRIGHT_CYAN 11
-#define BRIGHT_RED 12
-#define BRIGHT_MAGENTA 13
-#define BRIGHT_YELLOW 14
-#define BRIGHT_WHITE 15
+#define INDEX_BLACK 0
+#define INDEX_BLUE 1
+#define INDEX_GREEN 2
+#define INDEX_CYAN 3
+#define INDEX_RED 4
+#define INDEX_MAGENTA 5
+#define INDEX_BROWN 6
+#define INDEX_LIGHT_GRAY 7
+#define INDEX_DARK_GRAY 8
+#define INDEX_BRIGHT_BLUE 9
+#define INDEX_BRIGHT_GREEN 10
+#define INDEX_BRIGHT_CYAN 11
+#define INDEX_BRIGHT_RED 12
+#define INDEX_BRIGHT_MAGENTA 13
+#define INDEX_BRIGHT_YELLOW 14
+#define INDEX_BRIGHT_WHITE 15
 
 // size of one tile in px 
 #define TILE_W 8 
