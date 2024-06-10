ec0c07b81f3e8a874d76dbf593115e32af9fc70c
AslD <122040070+Ztry8@users.noreply.github.com>
2024-06-10 19:39:11 +0600

    Update core.c

--- core.c
+++ core.c
@@ -5,27 +5,22 @@
 // scale of graphics
 #define SCALE 2
 
-const unsigned char BLACK[3] = { 0, 0, 0 };				// 0
-const unsigned char GRAY[3] = { 85, 85, 85 };			// 1 
-const unsigned char WHITE[3] = { 255, 255, 255 };		// 2
-
-const unsigned char RED[3] = { 255, 0, 0 };				// 3
-const unsigned char CORAL[3] = { 255, 85, 85 };			// 4
-const unsigned char ORANGE[3] = { 255, 170, 0 };		// 5
-
-const unsigned char GREEN[3] = { 0, 255, 0 };			// 6
-const unsigned char DARK_GREEN[3] = { 0, 85, 0 };		// 7
-const unsigned char FOREST_GREEN[3] = { 85, 170, 0 };	// 8
-
-const unsigned char BLUE[3] = { 0, 0, 255 };			// 9
-const unsigned char CYAN[3] = { 0, 85, 85 };			// 10
-const unsigned char AQUA[3] = { 0, 255, 255 };			// 11
-const unsigned char EMERALD[3] = { 0, 255, 170 };		// 12
-
-const unsigned char YELLOW[3] = { 255, 255, 0 };		// 13
-
-const unsigned char PINK[3] = { 255, 0, 255 };			// 14
-const unsigned char PURPLE[3] = { 170, 0, 170 };		// 15
+const unsigned char BLACK[3] = { 0, 0, 0 };		
+const unsigned char BLUE[3] = { 0, 0, 170 };	
+const unsigned char GREEN[3] = { 0, 170, 0 };	
+const unsigned char CYAN[3] = { 0, 170, 170 };	
+const unsigned char RED[3] = { 170, 0, 0 };	
+const unsigned char MAGENTA[3] = { 170, 0, 170 };
+const unsigned char BROWN[3] = { 170, 85, 0 };
+const unsigned char LIGHT_GRAY[3] = { 170, 170, 170 };	
+const unsigned char DARK_GRAY[3] = { 85, 85, 85 };	
+const unsigned char BRIGHT_BLUE[3] = { 85, 85, 255 };	
+const unsigned char BRIGHT_GREEN[3] = { 85, 255, 85 };	
+const unsigned char BRIGHT_CYAN[3] = { 85, 255, 255 };	
+const unsigned char BRIGHT_RED[3] = { 255, 85, 85 };
+const unsigned char BRIGHT_MAGENTA[3] = { 255, 85, 255 };
+const unsigned char BRIGHT_YELLOW[3] = { 255, 255, 85 };
+const unsigned char BRIGHT_WHITE[3] = { 255, 255, 255 };		
 
 // init core of engine
 unsigned char init_core(struct Core* core) {
@@ -36,11 +31,10 @@ unsigned char init_core(struct Core* core) {
 		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error!", buffer, NULL);
 		return 1;
 	}
-	SDL_GetDesktopDisplayMode(0,&displayMode);
 
 	SDL_ShowCursor(0);
 	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "0");
-	SDL_SetHint(SDL_HINT_APP_NAME, "Province");
+	SDL_SetHint(SDL_HINT_APP_NAME, "Engine");
 	SDL_SetHint(SDL_HINT_RENDER_BATCHING, "1");
 	SDL_SetHint(SDL_HINT_GRAB_KEYBOARD, "1");
 	SDL_SetHint(SDL_HINT_RENDER_VSYNC, "0");
@@ -48,7 +42,6 @@ unsigned char init_core(struct Core* core) {
 
 	core->window = SDL_CreateWindow("Province", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
 		WIDTH * TILE_W * SCALE, HEIGHT * TILE_H * SCALE, 0);
-	//core->window = SDL_CreateWindow("Province", 0, 0, displayMode.w, displayMode.h, 0);	
 	if (core->window == NULL) {
 		char buffer[128];
 		sprintf_s(buffer, 128, "Window couldn't be created!\nError: %s", SDL_GetError());
@@ -72,7 +65,7 @@ unsigned char init_core(struct Core* core) {
 
 	if (raw_gfx == NULL) {
 		char buffer[128];
-		sprintf_s(buffer, 128, "Assets couldn't be loaded!\nTry reinstalling the game or installing the original, not the modified version!\nError: %s", SDL_GetError());
+		sprintf_s(buffer, 128, "Assets couldn't be loaded!\nError: %s", SDL_GetError());
 		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error!", buffer, NULL);
 		return 1;
 	}
@@ -80,13 +73,13 @@ unsigned char init_core(struct Core* core) {
 	core->gfx = SDL_CreateTextureFromSurface(core->renderer, raw_gfx); SDL_FreeSurface(raw_gfx);
 	if (core->gfx == NULL) {
 		char buffer[128];
-		sprintf_s(buffer, 128, "Assets couldn't initialize!\nTry reinstalling the game or installing the original, not the modified version!\nError: %s", SDL_GetError());
+		sprintf_s(buffer, 128, "Assets couldn't initialize!\nError: %s", SDL_GetError());
 		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error!", buffer, NULL);
 		return 1;
 	}
 
 	if (init_game() != 0) {
-		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Your system doesn't meet the requirements!\nYou need 2GB of free RAM and more to run!", NULL);
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Your system doesn't meet the requirements!", NULL);
 		return 1;
 	}
 
@@ -94,22 +87,22 @@ unsigned char init_core(struct Core* core) {
 }
 
 static const unsigned char* set_color(unsigned char color) {
-	if (color == 1) return GRAY;
-	else if (color == 2) return WHITE;
-	else if (color == 3) return RED;
-	else if (color == 4) return CORAL;
-	else if (color == 5) return ORANGE;
-	else if (color == 6) return GREEN;
-	else if (color == 7) return DARK_GREEN;
-	else if (color == 8) return FOREST_GREEN;
-	else if (color == 9) return BLUE;
-	else if (color == 10) return CYAN;
-	else if (color == 11) return AQUA;
-	else if (color == 12) return EMERALD;
-	else if (color == 13) return YELLOW;
-	else if (color == 14) return PINK;
-	else if (color == 15) return PURPLE;
-	else return BLACK;
+	if (color == 0) return BLACK;
+	else if (color == 1) return BLUE;
+	else if (color == 2) return GREEN;
+	else if (color == 3) return CYAN;
+	else if (color == 4) return RED;
+	else if (color == 5) return MAGENTA;
+	else if (color == 6) return BROWN;
+	else if (color == 7) return LIGHT_GRAY;
+	else if (color == 8) return DARK_GRAY;
+	else if (color == 9) return BRIGHT_BLUE;
+	else if (color == 10) return BRIGHT_GREEN;
+	else if (color == 11) return BRIGHT_CYAN;
+	else if (color == 12) return BRIGHT_RED;
+	else if (color == 13) return BRIGHT_MAGENTA;
+	else if (color == 14) return BRIGHT_YELLOW;
+	else return BRIGHT_WHITE;
 }
 
 void set_tile(struct Core* core, char symbol, unsigned char color, signed long x, signed long y) {
@@ -132,7 +125,7 @@ void run_core(struct Core* core) {
 		unsigned long long start = SDL_GetPerformanceCounter();
 		while (SDL_PollEvent(&e)) { 
 			if (e.type == SDL_QUIT || SDL_GetKeyboardState(NULL)[SDL_SCANCODE_ESCAPE]) quit = 1; 
-			if (/*e.key.repeat == 0 &&*/ e.type == SDL_KEYDOWN) input_game(e.key.keysym.scancode);
+			if (e.type == SDL_KEYDOWN) input_game(e.key.keysym.scancode);
 		}
 
 		SDL_RenderClear(core->renderer);
